0eba01c768da32cd057fd99087992fd4947f7198
urmasmuld <urmas.muldmaa@gmail.com>
2022-02-17 13:00:25 +0200

    new port for server (3000>3001)

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res)=>{
     res.end('Hello');
 });
 
-server.listen(3000,()=>{
-    console.log('Server started on http://localhost:3000');
+server.listen(3001,()=>{
+    console.log('Server started on http://localhost:3001');
 });
